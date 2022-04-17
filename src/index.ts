7a9943230d15c4598636dd2fb931329d807514f4
Paul Law <paul@80nd.co.uk>
2022-04-17 10:56:17 +0100

    expose StmtSetPC

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export { Program, Line, Label, StmtInsn, Node, Operand, Literal, Register, ScopeQualifiedIdent, Loc, SourceLoc } from './ast';
+export {
+  Program, Line, Label,
+  StmtInsn, StmtSetPC, Node,
+  Operand, Literal, Register, ScopeQualifiedIdent,
+  Loc, SourceLoc
+} from './ast';
 export { assemble, parseOnly, Diagnostic } from './asm';
 export { disassemble } from './disasm';
