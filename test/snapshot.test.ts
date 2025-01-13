3d226c72adca4c49397abca3c05ba552b2efa9e4
Paul Law <paul@80nd.co.uk>
2025-01-13 21:12:28 +0000

    fix glob snapshot tests

--- test/snapshot.test.ts
+++ test/snapshot.test.ts
@@ -1,7 +1,6 @@
 // Closely inspired by https://github.com/nurpax/c64jasm/blob/master/test/test.ts
 
-const glob = require('glob').glob;
-
+import { glob } from 'glob'
 import * as assert from 'assert';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -18,45 +17,43 @@ suite('rcasm - Snapshots', () => {
 
   test('Output', function () {
 
-    glob("test/cases/*.input.asm", {}, (err: Error, files: string[]) => {
+    glob.sync("test/cases/*.input.asm").forEach(fname => {
+      const src = fs.readFileSync(fname).toString();
+      const lines = src.split('\n');
+      const disasmOptions: DisasmOptions = {
+      };
 
-      files.forEach(fname => {
-        const src = fs.readFileSync(fname).toString();
-        const lines = src.split('\n');
-        const disasmOptions: DisasmOptions = {
-        };
+      const { prg, errors, debugInfo } = assemble(src);
 
-        const { prg, errors, debugInfo } = assemble(src);
-
-        if (lines.length > 0) {
-          const match = /;\s+disasm:\s+(.*)/.exec(lines[0]);
-          if (match !== null) {
-            const opts = match[1].split(' ');
-            if (opts.includes('debuginfo')) {
-              disasmOptions.isInstruction = debugInfo?.info().isInstruction;
-            }
+      if (lines.length > 0) {
+        const match = /;\s+disasm:\s+(.*)/.exec(lines[0]);
+        if (match !== null) {
+          const opts = match[1].split(' ');
+          if (opts.includes('debuginfo')) {
+            disasmOptions.isInstruction = debugInfo?.info().isInstruction;
           }
         }
+      }
 
-        if (errors.length > 0) {
-          console.error(errors);
-          assert.fail();
-        }
+      if (errors.length > 0) {
+        console.error(errors);
+        assert.fail();
+      }
 
-        const disasmLines = disassemble(prg /*, undefined*/, disasmOptions).concat('');
-        const expectedFname = path.join(path.dirname(fname), path.basename(fname, 'input.asm') + 'expected.asm');
-        const actualFname = path.join(path.dirname(fname), path.basename(fname, 'input.asm') + 'actual.asm');
+      const disasmLines = disassemble(prg /*, undefined*/, disasmOptions).concat('');
+      const expectedFname = path.join(path.dirname(fname), path.basename(fname, 'input.asm') + 'expected.asm');
+      const actualFname = path.join(path.dirname(fname), path.basename(fname, 'input.asm') + 'actual.asm');
 
-        // If the expected file doesn't exist, create it.  This is for new test authoring.
-        if (!fs.existsSync(expectedFname)) {
-          fs.writeFileSync(expectedFname, disasmLines.join('\n'));
-        }
+      // If the expected file doesn't exist, create it.  This is for new test authoring.
+      if (!fs.existsSync(expectedFname)) {
+        fs.writeFileSync(expectedFname, disasmLines.join('\n'));
+      }
 
-        const expectedLines = readLines(expectedFname);
-        for (let lineIdx = 0; lineIdx < expectedLines.length; lineIdx++) {
-          if (expectedLines[lineIdx].trim() !== disasmLines[lineIdx].trim()) {
-            fs.writeFileSync(actualFname, disasmLines.join('\n'));
-            console.error(`Test failed.
+      const expectedLines = readLines(expectedFname);
+      for (let lineIdx = 0; lineIdx < expectedLines.length; lineIdx++) {
+        if (expectedLines[lineIdx].trim() !== disasmLines[lineIdx].trim()) {
+          fs.writeFileSync(actualFname, disasmLines.join('\n'));
+          console.error(`Test failed.
 Input .asm:
 
 cat ${fname}
@@ -76,11 +73,9 @@ To gild to actual output:
 cp ${actualFname} ${expectedFname}
 
 `);
-            assert.fail(`Test ${fname} failed`);
-          }
+          assert.fail(`Test ${fname} failed`);
         }
-      });
-
+      }
     });
 
   });
