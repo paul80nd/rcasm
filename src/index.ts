4cb947a7fbe57018ca0e73c566f126f3603fced1
Paul Law <paul@80nd.co.uk>
2025-11-03 21:50:31 +0000

    expose types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export {
   Node, Loc, SourceLoc,
   Program, Line, Label,
-  Stmt, StmtAlign, StmtData, StmtError, StmtFill, StmtFor, StmtLet, StmtIfElse, StmtInsn, StmtSetPC,
-  Expr, BinaryOp,
+  Stmt, StmtAlign, StmtData, StmtError, StmtFill, StmtFor, StmtLet, StmtIfElse, StmtInsn, StmtSetPC, DataSize,
+  Expr, BinaryOp, CallFunc,
   Literal, Register, Ident, ScopeQualifiedIdent, GetCurPC
 } from './ast';
 export { assemble, parseOnly, Diagnostic } from './asm';
